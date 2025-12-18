f810be4a370df4901beacad2407d0b3bff4a8a0d
kseeed <kseeed@yandex.ru>
2025-12-18 18:52:23 +0300

    js have been updated

--- src/components/searching.js
+++ src/components/searching.js
@@ -3,9 +3,13 @@ import {rules, createComparison} from "../lib/compare.js";
 
 export function initSearching(searchField) {
     // @todo: #5.1 — настроить компаратор
+    const skipEmptyTargetValuesRule = rules.skipEmptyTargetValues();
+    const searchRule = rules.searchMultipleFields(searchField, ['date', 'customer', 'seller'], false);
+    const compare = createComparison(['skipEmptyTargetValues'], [searchRule]);
 
     return (data, state, action) => {
         // @todo: #5.2 — применить компаратор
-        return data;
+        return data.filter(row => compare(row, state));
+        // return data;
     }
 }
